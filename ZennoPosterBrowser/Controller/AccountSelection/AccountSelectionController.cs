139c297078f88d39118d5fccd87a3bf137497d54
Disaylt <disofban@gmail.com>
2022-08-12 17:21:29 +0300

    create form controller

---
+++ ZennoPosterBrowser/Controller/AccountSelection/AccountSelectionController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZennoPosterBrowser.Controller.AccountSelection
+{
+    internal class AccountSelectionController : FormController
+    {
+        private const string _nameForm = "Выбор аккаунтов";
+        public AccountSelectionController() : base(_nameForm)
+        {
+
+        }
+    }
+}
