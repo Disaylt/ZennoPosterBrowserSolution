0fbf3745fc61258c3af669cdd2051b388f6fa7ab
Disaylt <disofban@gmail.com>
2022-08-17 16:11:49 +0300

    create markets collection

--- ZennoPosterBrowser/Mongo/BrowserCollections/MarketsCollection.cs
+++ ZennoPosterBrowser/Mongo/BrowserCollections/MarketsCollection.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ZennoPosterBrowser.Models.BSON;
 
 namespace ZennoPosterBrowser.Mongo.BrowserCollections
 {
-    internal class MarketsCollection
+    internal class MarketsCollection : BrowserDatabaseConnector, IMongoCollectionConnector<ProjectNameModel>
     {
+        private const string _collectionName = "Markets";
+
+        public MarketsCollection()
+        {
+            Collection = DataBase.GetCollection<ProjectNameModel>(_collectionName);
+        }
+
+        public IMongoCollection<ProjectNameModel> Collection { get; }
     }
 }
--- ZennoPosterBrowser/Mongo/BrowserDatabaseConnector.cs
+++ ZennoPosterBrowser/Mongo/BrowserDatabaseConnector.cs
@@ -7,11 +7,19 @@ using System.Threading.Tasks;
 
 namespace ZennoPosterBrowser.Mongo
 {
-    internal class BrowserDatabaseConnector
+    internal class BrowserDatabaseConnector : MongoConnector
     {
         private const string _databaseName = "ZennoposterBrowser";
         private static IMongoDatabase _mongoDatabase;
 
+        public BrowserDatabaseConnector()
+        {
+            if(_mongoDatabase == null)
+            {
+                _mongoDatabase = GetDatabase(_databaseName);
+            }
+        }
 
+        public IMongoDatabase DataBase => _mongoDatabase;
     }
 }
