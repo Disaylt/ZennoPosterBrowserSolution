bbd9f2a38b6c0403ccdba9236fa9a19ebebc4fca
Disaylt <disofban@gmail.com>
2022-08-19 16:35:45 +0300

    create mongo account settings connector

---
+++ ZennoPosterBrowser/Models/BSON/AccountsSettingsModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZennoPosterBrowser.Models.BSON
+{
+    internal class AccountsSettingsModel
+    {
+        public string MarketName { get; set; }
+        public string ProjectName { get; set; }
+        public string DataBase { get; set; }
+        public string Collection { get; set; }
+        public string FolderPath { get; set; }
+    }
+}
---
+++ ZennoPosterBrowser/Mongo/BrowserCollections/AccountsSettingsCollections.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZennoPosterBrowser.Models.BSON;
+
+namespace ZennoPosterBrowser.Mongo.BrowserCollections
+{
+    internal class AccountsSettingsCollections : BrowserDatabaseConnector, IMongoCollectionConnector<MarketNameModel>
+    {
+        private const string _collectionName = "AccountsSettings";
+
+        public AccountsSettingsCollections()
+        {
+            Collection = DataBase.GetCollection<MarketNameModel>(_collectionName);
+        }
+
+        public IMongoCollection<MarketNameModel> Collection { get; }
+    }
+}
