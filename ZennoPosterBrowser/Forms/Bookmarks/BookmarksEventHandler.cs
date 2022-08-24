ed9bc9959e3efb1eb08ea9b543c0e5a4be6b7875
Disaylt <disofban@gmail.com>
2022-08-24 19:35:01 +0300

    create: add update bookmarks

--- ZennoPosterBrowser/Forms/Bookmarks/BookmarksEventHandler.cs
+++ ZennoPosterBrowser/Forms/Bookmarks/BookmarksEventHandler.cs
@@ -25,6 +25,7 @@ namespace ZennoPosterBrowser.Forms.Bookmarks
         {
             _bookmarksFormControls.AdditionBookmark.Click += AddBookmark;
             _bookmarksFormControls.DeletionBookmark.Click += DeleteBookmark;
+            _bookmarksFormControls.UpdationBookmark.Click += UpdateBookmark;
             _bookmarksFormControls.BookmarketComboBox.SelectedIndexChanged += ChangeBookmarkName;
         }
 
@@ -70,6 +71,21 @@ namespace ZennoPosterBrowser.Forms.Bookmarks
                     .FirstOrDefault(x=> x.Name == bookmark)
                     .Url ?? string.Empty;
                 _bookmarksFormControls.BookmarkCurrentUrl.Text = url;
+            }
+        }
+
+        protected virtual void UpdateBookmark(object sender, EventArgs e)
+        {
+            if (_bookmarksFormControls.BookmarketComboBox.SelectedItem is string bookmarkName
+                && !string.IsNullOrEmpty(bookmarkName)
+                && _bookmarksForm.BookmarksStorage.Bookmarks.Any(x=> x.Name == bookmarkName))
+            {
+                BookmarkModel bookmark = new BookmarkModel
+                {
+                    Name = bookmarkName,
+                    Url = _bookmarksFormControls.BookmarkCurrentUrl.Text
+                };
+                _bookmarksForm.BookmarksStorage.UpdateBookmark(bookmark);
             }
         }
     }
