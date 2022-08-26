661c8dd5c9101839a7ca2167e6af5dbbeed74838
Disaylt <disofban@gmail.com>
2022-08-26 14:50:13 +0300

    create: add accountCreatoraction for used service

--- ZennoPosterBrowser/Forms/AccountCreator/AccountCreatorForm.cs
+++ ZennoPosterBrowser/Forms/AccountCreator/AccountCreatorForm.cs
@@ -4,6 +4,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ZennoPosterBrowser.Configs;
 using ZennoPosterBrowser.Forms.Base;
 
 namespace ZennoPosterBrowser.Forms.AccountCreator
@@ -14,12 +15,13 @@ namespace ZennoPosterBrowser.Forms.AccountCreator
 
         public AccountCreatorForm()
         {
+            NextAction = BrowserProjectActions.CloseBrowser;
             Form.Size = new Size(400, 500);
             Form.Name = _nameForm;
             FormControls = new AccountCreatorFormControls();
             AddControls(FormControls);
         }
-
+        public BrowserProjectActions NextAction { get; set; }
         public AccountCreatorFormControls FormControls { get; }
     }
 }
---
+++ ZennoPosterBrowser/Forms/AccountCreator/AccountCreatorFormBrowserAction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZennoPosterBrowser.Configs;
+using ZennoPosterBrowser.Services.BrowserActions;
+
+namespace ZennoPosterBrowser.Forms.AccountCreator
+{
+    internal class AccountCreatorFormBrowserAction : IBrowserAction
+    {
+        private AccountCreatorForm _accountCreatorForm;
+        public AccountCreatorFormBrowserAction()
+        {
+            _accountCreatorForm = new AccountCreatorForm();
+        }
+
+        public BrowserProjectActions Run()
+        {
+            try
+            {
+                _accountCreatorForm.Form.ShowDialog();
+                return _accountCreatorForm.NextAction;
+            }
+            catch (Exception)
+            {
+                return BrowserProjectActions.CloseBrowser;
+            }
+        }
+    }
+}
