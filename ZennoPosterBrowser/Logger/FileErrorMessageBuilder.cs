be370cd4bc3f130a1f9c32471f6374b144718b74
disaylt <disofban@gmail.com>
2022-08-29 13:50:33 +0300

    feat: add abstract logger messagers and file logger

---
+++ ZennoPosterBrowser/Logger/FileErrorMessageBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZennoPosterBrowser.Logger
+{
+    internal class FileErrorMessageBuilder : ErrorMessage
+    {
+        public FileErrorMessageBuilder(Exception exception) : base(exception)
+        {
+        }
+
+        public override string GetMessage()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            string firstAndLastLine = "----------------------------";
+            stringBuilder.AppendLine(firstAndLastLine);
+            stringBuilder.AppendLine("Error");
+            stringBuilder.AppendLine($"Date: {DateTime.Now}");
+            stringBuilder.AppendLine($"Message: {Exception.Message}");
+            stringBuilder.AppendLine($"StackTrace: {Exception.StackTrace}");
+            stringBuilder.AppendLine(firstAndLastLine);
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ ZennoPosterBrowser/Logger/FileInfoMessageBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZennoPosterBrowser.Logger
+{
+    internal class FileInfoMessageBuilder : InfoMessage
+    {
+        public FileInfoMessageBuilder(string message) : base(message)
+        {
+
+        }
+
+        public override string GetMessage()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            string firstAndLastLine = "----------------------------";
+            stringBuilder.AppendLine(firstAndLastLine);
+            stringBuilder.AppendLine("INFO");
+            stringBuilder.AppendLine($"Date: {DateTime.Now}");
+            stringBuilder.AppendLine($"Message: {Message}");
+            stringBuilder.AppendLine(firstAndLastLine);
+            return stringBuilder.ToString();
+        }
+    }
+}
